45aef240d0a913189bb17aa99e083c8a99e42310
Jason Knoll <jason.p.knoll@gmail.com>
2021-06-15 21:10:45 -0700

    Iteration1 fix class methods

--- lib/exhibit.rb
+++ lib/exhibit.rb
@@ -1,9 +1,9 @@
 class Exhibit
   attr_reader :name, :cost
 
-  def intitialize(name, cost)
-    @name = name
-    @cost = cost
+  def initialize(attributes)
+    @name = attributes[:name]
+    @cost = attributes[:cost]
   end
 
 end
--- lib/patron.rb
+++ lib/patron.rb
@@ -1,7 +1,7 @@
 class Patron
   attr_reader :name, :spending_money, :interests
 
-  def intitialize(name, spending_money)
+  def initialize(name, spending_money)
     @name = name
     @spending_money = spending_money
     @interests = []
